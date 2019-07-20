c71a22a05870714427ac58b0030612cc14fd6a5d
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2019-07-20 12:00:07 +0300

    added TestRun class to execute test without running jetty server

---
+++ src/main/java/il/ac/bgu/bp/cotextualBlockly/TestRun.java
@@ -0,0 +1,9 @@
+package il.ac.bgu.bp.cotextualBlockly;
+
+public class TestRun {
+  private static RunServlet runServlet = new RunServlet();
+
+  public static void main(String[] args) throws Exception {
+    runServlet.reset();
+  }
+}
